ce2ca43157b1a0cd8d9d3c5996557e42540eaf9c
Kawê Cezar <kawecezarjesus@gmail.com>
2025-01-17 06:29:05 -0500

    Aula 22 de JavaScrit - Funções Parametrizadas

---
+++ aula22/script.js
@@ -0,0 +1,20 @@
+
+
+let valor  = 0
+
+console.log(valor)
+
+function add(v) {
+    return valor + v
+}
+valor = add(10)
+
+console.log(valor)
+
+
+
+function soma(p1) {
+  console.log(p1)
+}
+
+//soma("Kawê")
