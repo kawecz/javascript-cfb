be0fee38a3ba2b4a830ccd48210646e35729ed53
Kawê Cezar <kawecezarjesus@gmail.com>
2025-01-17 05:46:18 -0500

    aula 20 do curso de js

--- aula020/script.js
+++ aula020/script.js
@@ -1,4 +1,17 @@
 
+function mudartexto() {
+    let d1 = document.querySelector("#d1")
+    let d2 = document.querySelector("#d2")
+    let d3 = document.querySelector("#d3")
+
+    d1.innerHTML = `CFB Cursos`
+    d2.innerHTML = `CFB Cursos`
+    d3.innerHTML = `CFB Cursos`
+}
+
+
+
+
 function nome() {
     console.log("CFB cursos")
 }
