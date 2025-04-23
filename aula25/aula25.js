196aaed4b504946f0828989c7495fd927df0e991
Kawe Cezar <kawecezarjesus@gmail.com>
2025-04-23 14:51:53 -0500

    aula 25 do curso de javascript: arrow functions

---
+++ aula25/aula25.js
@@ -0,0 +1,17 @@
+//const soma = function(v1,v2) {return v1 +v2}
+
+//const operacoes = (v1, v2) => {
+//let soma = v1 + v2;
+//let multi = v1 * v2;
+//return soma, multi;
+//};
+//console.log(operacoes(10, 5));
+
+//const add = (n) => n + 10;
+//console.log(add(10));
+
+//console.log("Hello, my buddy!");
+const soma = (v1, v2) => {
+  return v1 + v2;
+};
+console.log("resultado da soma:", soma(10, 10));
