13cca42163f21794f007db8d2d219f649b827daf
kawecz <kawecezarjesus@gmail.com>
2025-09-30 15:02:38 -0500

    Aula 56 - Template Strings em Javascript

---
+++ aula56/script.js
@@ -0,0 +1,9 @@
+const caixa = document.querySelector("#caixa")
+
+
+const curso = "JavaScript"
+const canal = "CFB Cursos"
+
+const frase = `<p>Este é o curso de ${curso} do ${canal}</p>`
+
+caixa.innerHTML = frase
