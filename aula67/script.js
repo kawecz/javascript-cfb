95038d3b4c7c6ad96f3154d49870210015b8d2a2
unknown <kawecezarjesus@gmail.com>
2026-01-27 18:14:21 -0500

    Aula 67 - Aprendendo sobre PROTOTYPE

---
+++ aula67/script.js
@@ -0,0 +1,12 @@
+const Nave = function(energia) {
+    this.energia = energia
+    this.disparos = 100
+}
+
+const n1 = new Nave(100)
+
+Nave.prototype.vidas = 3
+
+console.log(Nave)
+console.log(n1)
+console.log(n1.vidas)
